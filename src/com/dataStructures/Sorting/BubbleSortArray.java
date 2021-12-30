108bc288525a7c0f3bbff819cfd3d4e0d9167487
Dylan LaMott <66742430+GothamsJoker@users.noreply.github.com>
2021-12-29 21:12:44 -0500

    explain bubble sort

--- src/com/dataStructures/Sorting/BubbleSortArray.java
+++ src/com/dataStructures/Sorting/BubbleSortArray.java
@@ -21,17 +21,22 @@ public class BubbleSortArray {
         }
     }
 
-    private void swap(int one, int two){
-        long temp = a[one];
-        a[one] = a[two];
-        a[two] = temp;
-    }
-
+    /***
+     * Bubble sort is implemented by starting a sort where the smallest element is placed at the
+     * start of an array and the largest is placed at the end a.length-1;. The outer loop starts
+     * at the end of an array if items are greater than OUT will be sorted. The inner loop starts
+     * at the beginning of an array exiting when out is reached. in and in+1 are compared and
+     * swapped if "in" is greater than "in+1". Bubble swap is slow and inefficient for large
+     * arrays. O(n2) speed.
+     */
     public void bubbleSort(){
+
         for(int out = numberOfElement-1; out > 1; out--){
             for(int in = 0; in < out; in++){
                 if(a[in] > a[in+1]){
-                    swap(in, in +1);
+                    long temp = a[in];
+                    a[in] = a[in+1];
+                    a[in+1] = temp;
                 }
             }
         }
