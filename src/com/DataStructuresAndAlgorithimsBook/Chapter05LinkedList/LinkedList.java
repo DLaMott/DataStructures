75aed06f22fe0c4f60627b20d21533ba328f52a9
GothamsJoker <dylanlamott@gmail.com>
2022-02-21 17:04:17 -0500

    Linked list

--- src/com/DataStructuresAndAlgorithimsBook/Chapter05LinkedList/LinkedList.java
+++ src/com/DataStructuresAndAlgorithimsBook/Chapter05LinkedList/LinkedList.java
@@ -4,4 +4,36 @@ package com.DataStructuresAndAlgorithimsBook.Chapter05LinkedList;
  *
  */
 public class LinkedList {
+
+    private Link first;
+
+    public void LinkList(){
+        first = null;
+    }
+
+    public boolean isEmpty(){
+        return (first==null);
+    }
+
+    public void insertFirst(int id, double dd){
+        Link newLink = new Link(id, dd);
+        newLink.next = first;
+        first = newLink;
+    }
+
+    public Link deleteFirst(){
+        Link temp = first;
+        first = first.next;
+        return temp;
+    }
+
+    public void displayList(){
+        System.out.print("List (first ->last): ");
+        Link current = first;
+        while (current != null){
+            current.displayLink();
+            current = current.next;
+        }
+        System.out.println("");
+    }
 }
