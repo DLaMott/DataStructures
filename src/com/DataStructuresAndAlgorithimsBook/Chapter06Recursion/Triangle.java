d8d53904a1011d6909302fd78f47b480d60e4de5
GothamsJoker <dylanlamott@gmail.com>
2022-02-24 22:12:25 -0500

    Setting up chapter 6

---
+++ src/com/DataStructuresAndAlgorithimsBook/Chapter06Recursion/Triangle.java
@@ -0,0 +1,36 @@
+package com.DataStructuresAndAlgorithimsBook.Chapter06Recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Triangle {
+    static int theNumber;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter a number: ");
+        theNumber = getInt();
+        int theAnswer = triangle(theNumber);
+        System.out.println("Triangle = " + theAnswer);
+    }
+
+    public static int triangle(int n){
+        //Base case where n == 1 ending recursion
+        if(n==1){
+            return 1;
+        }else{
+            // Recursion begins
+            return (n + triangle(n-1));
+        }
+    }
+
+    public static int getInt() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        int number = Integer.parseInt(s);
+        return number;
+    }
+
+
+}
